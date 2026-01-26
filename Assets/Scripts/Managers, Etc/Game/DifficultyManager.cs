46f6b35cdeb22a9ca033b066ca209d792fda2c24
KhaledKhalil420 <ko.eyg2001@gmail.com>
2026-01-26 04:14:47 +0200

    DifficultyManager

---
+++ Assets/Scripts/Managers, Etc/Game/DifficultyManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Difficulty : MonoBehaviour
+{
+    public static int DifficultyMultiplier = 1;
+
+    public static void IncreaseDifficulty(int Difficulty)
+    {
+        DifficultyMultiplier += Difficulty;
+    }
+}
+    
