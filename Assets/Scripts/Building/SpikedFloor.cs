a124cf6a793c5bbd9d80714292136f643278f74c
KhaledKhalil420 <ko.eyg2001@gmail.com>
2026-02-14 14:04:31 +0200

    Spikes hit feedback (Polish)

--- Assets/Scripts/Building/SpikedFloor.cs
+++ Assets/Scripts/Building/SpikedFloor.cs
@@ -1,28 +1,57 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class SpikedFloor : Building
 {
     [SerializeField] private int damage;
-    [SerializeField] private float damageInterval = 0.5f;
+    [SerializeField] private float damageInterval = 1f;
+    [SerializeField] private SkinnedMeshRenderer skinnedMesh;
+    [SerializeField] private AudioSource spikesSource;
     
     private Collider[] hitBuffer = new Collider[10];
     private float nextDamageTime;
     private Vector3 halfExtents;
+    private bool isReady = true;
+    private Tween currentTween;
 
     void Start()
     {
-        halfExtents = transform.localScale * 0.1f;
+        halfExtents = skinnedMesh.bounds.extents;
+        skinnedMesh.SetBlendShapeWeight(0, 0f);
     }
 
     void Update()
     {
-        if (Time.time < nextDamageTime) return;
+        if (!isReady)
+        {
+            if (Time.time >= nextDamageTime)
+            {
+                isReady = true;
+            }
+            return;
+        }
         
         int hitCount = Physics.OverlapBoxNonAlloc(transform.position, halfExtents, hitBuffer, Quaternion.identity, LayerMask.GetMask("Enemy"));
         
-        for (int i = 0; i < hitCount; i++)
-            if (hitBuffer[i].TryGetComponent(out IDamagable damagable)) damagable.Damage(damage + extraDamage);
-        
-        if (hitCount > 0) { Damage(1); nextDamageTime = Time.time + damageInterval; }
+        if (hitCount > 0) 
+        { 
+            for (int i = 0; i < hitCount; i++)
+                if (hitBuffer[i].TryGetComponent(out IDamagable damagable)) damagable.Damage(damage + extraDamage);
+            
+            Damage(1);
+            isReady = false;
+            nextDamageTime = Time.time + damageInterval;
+            
+            currentTween?.Kill();
+            skinnedMesh.SetBlendShapeWeight(0, -100f);
+
+            currentTween?.Kill();
+            currentTween = DOTween.To(() => skinnedMesh.GetBlendShapeWeight(0), x => skinnedMesh.SetBlendShapeWeight(0, x), 0f, damageInterval * 1.5f).SetEase(Ease.OutQuad).SetRecyclable(true);
+            
+            spikesSource.pitch = Random.Range(0.95f, 1.05f);
+            spikesSource.Play();
+        }
     }
+
+    void OnDestroy() => currentTween?.Kill();
 }
